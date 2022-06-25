c54bdffc3a72d79c1eb4beff5f4c1976c2094d8f
YipChiSan <zye39@yahoo.com>
2022-06-25 21:57:01 +1000

    Finish Initialisation

---
+++ my-component-code/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MyStepper from "./Component/MyStepper";
+
+function App() {
+  return (
+    <div className="App">
+      <MyStepper />
+      
+    </div>
+  );
+}
+
+export default App;
